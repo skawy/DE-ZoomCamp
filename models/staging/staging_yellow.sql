e88a73f3d187fdc54a86d2b992cc184cd5a03ade
ahmed skawy <elskawyahmed1@gmail.com>
2023-12-03 15:02:52 +0000

    finish dbt modeling and testing

--- models/staging/staging_yellow.sql
+++ models/staging/staging_yellow.sql
@@ -1,22 +1,24 @@
 {{ config (materialized = 'view')}}
 
 select
-    -- identifiers
-    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+   
+   -- identifiers
+    {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
     
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -24,7 +26,7 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
@@ -32,7 +34,7 @@ select
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
 
-FROM {{ source("resolute-choir-403411",'green')}}
+FROM {{ source("resolute-choir-403411",'yellow')}}
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
